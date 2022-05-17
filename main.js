43acd6c7646c98d97e309e9994fca1ead4a37d3d
foreigndemon <nikhil123omkar@gmail.com>
2022-05-17 16:12:03 +0530

    added confetti on winning the game

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const diceImage = document.querySelector('.game-menu-dice');
 const scoreHold = document.getElementById('score-hold')
 const playerHead = document.getElementById('player-heading');
 const cpuHead = document.getElementById('cpu-heading');
+const gameReset = document.getElementById('game-reset');
 
 const playerCurrentScore = document.getElementById('current-score-0');
 const cpuCurrentScore = document.getElementById('current-score-1');
@@ -16,6 +17,26 @@ let scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
+// confetti-intialization
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+
+function renderConfetti(){
+    confetti.render();
+}
+
+function clearConfetti(){
+    confetti.clear();
+}
+
+function switchActivePlayer(){
+    document.getElementById(`current-score-${activePlayer}`).textContent = 0 
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    playerHead.classList.toggle('player-active');
+    cpuHead.classList.toggle('player-active');
+}
+
 // rolling the dice
 rollDice.addEventListener('click',()=>{
     diceImage.classList.remove('hidden');
@@ -26,7 +47,23 @@ rollDice.addEventListener('click',()=>{
 
 scoreHold.addEventListener('click',()=>{
     scores[activePlayer] += currentScore;
-    document.getElementById(`score-player-${activePlayer}`)
+    document.getElementById(`score-player-${activePlayer}`).innerText = scores[activePlayer];
+    if(scores[activePlayer] >= 10){
+        renderConfetti();
+    }
+})
+
+gameReset.addEventListener('click',()=>{
+    activePlayer = 0;
+    currentScore = 0;
+    scores[0] = 0;
+    scores[1] = 0;
+    playerCurrentScore.innerText = 0;
+    cpuCurrentScore.innerText = 0;
+    document.querySelector('#score-player-0').innerText = 0;
+    document.querySelector('#score-player-1').innerHTML = 0;
+
+    clearConfetti();
 })
 
 function playerTurn(diceNumber){
@@ -34,11 +71,7 @@ function playerTurn(diceNumber){
         currentScore += diceNumber;
         document.getElementById(`current-score-${activePlayer}`).textContent = currentScore;
     }else{
-        document.getElementById(`current-score-${activePlayer}`).textContent = 0 
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        playerHead.classList.toggle('player-active');
-        cpuHead.classList.toggle('player-active');
+        switchActivePlayer();
     }
 }
 
